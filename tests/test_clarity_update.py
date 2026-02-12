635eb4e02248aaa119bc567bbe740aca35e49bef
Loofi <loofi@loofi.com>
2026-02-12 19:32:49 +0100

    test: sync clarity version assertions to 26.0.1

--- tests/test_clarity_update.py
+++ tests/test_clarity_update.py
@@ -24,11 +24,11 @@ class TestVersionModule(unittest.TestCase):
 
     def test_version_is_current(self):
         from version import __version__
-        self.assertEqual(__version__, "26.0.0")
+        self.assertEqual(__version__, "26.0.1")
 
     def test_version_codename(self):
         from version import __version_codename__
-        self.assertEqual(__version_codename__, "Plugin Marketplace")
+        self.assertEqual(__version_codename__, "Breadcrumb Bar UI Hotfix")
 
     def test_app_name(self):
         from version import __app_name__
